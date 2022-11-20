f31ba4cffa807871ca0cd088a3e85c07ee991b62
osteloniz30@gmail.com <osteloniz30@gmail.com>
2022-11-20 00:32:03 -0300

    DESIGN HOME E CALCULADORA

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbModalConfig, NgbModal]
+  
 })
 export class HomeComponent implements OnInit {
 
@@ -11,8 +15,13 @@ export class HomeComponent implements OnInit {
     {id: 1, marca: 'PagSeguro'}
   ];
 
-  constructor() { }
-
+  constructor(config: NgbModalConfig, private modalService: NgbModal) { 
+    config.backdrop = 'static';
+		config.keyboard = false;
+  }
+  open(content) {
+		this.modalService.open(content);
+	}
   ngOnInit(): void {
   }
 
