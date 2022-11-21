605bfc50a34176f2882fa7d4b55d8c112895c23c
João <ContactorGomes@protonmail.ch>
2022-11-20 23:15:05 -0300

    home finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CieloComponent } from './cielo/cielo.component';
 import { HomeComponent } from './home/home.component';
 import { MercadoPagoComponent } from './mercado-pago/mercado-pago.component';
+import { PagseguroComponent } from './pagseguro/pagseguro.component';
 import { CalculadoraComponent } from './share/calculadora/calculadora.component';
 import { PageNotFoundComponent } from './share/page-not-found/page-not-found.component';
 
@@ -9,6 +11,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'mp', component: MercadoPagoComponent},
+  { path: 'cielo', component: CieloComponent},
+  { path: 'pagseguro', component: PagseguroComponent},
   { path: 'calculadora', component: CalculadoraComponent},
   { path: '**', component: PageNotFoundComponent}
 
